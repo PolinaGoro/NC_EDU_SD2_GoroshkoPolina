69f26fde919e63a602784d08eec99818dbc18175
polli <poli.na.goroshk@gmail.com>
2019-10-24 02:52:01 +0300

    initial commit

---
+++ backend/src/test/java/com/netcracker/polina/bebackend/BeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.polina.bebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
